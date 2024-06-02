65a52c0abfd88d02e56f4a578f4f155fa5f10123
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 19:39:40 -0300

    feat/ criação do componente square

--- src/components/square/index.jsx
+++ src/components/square/index.jsx
@@ -1,7 +1,27 @@
-export function Square({ value, typeSelect, onClick, chidren }) {
+export function Square({
+  value,
+  disabled,
+  callBackClick,
+  label,
+  isSelect,
+  children,
+}) {
   return (
-    <article className="flex  size-18 items-center justify-center rounded bg-cinza-100 p-2">
-      {chidren}
-    </article>
+    <div className=" flex flex-col items-center">
+      <button
+        data-value={value}
+        aria-labelledby={`label-square-${label}`}
+        onClick={callBackClick}
+        className={`flex  size-18 items-center justify-center rounded-lg border border-cinza-100 bg-cinza-50 p-2  md:border-2 xl:border-[3px] ${isSelect && 'border-rosa-300'} hover:border-rosa-300`}
+      >
+        <div className=" pointer-events-none">{children}</div>
+      </button>
+      <p
+        className=" max-w-20 text-center text-ct2"
+        id={`label-square-${label}`}
+      >
+        {label}
+      </p>
+    </div>
   )
 }
