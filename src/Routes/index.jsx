01b9e14f485d91209b24c506b619c17900551b1d
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 19:43:03 -0300

    feat/ adicionando o contexto de produtps

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { StandardLayout } from '../layout/standardLayout'
 import { Home } from '../pages/home'
 import { Product } from '../pages/product'
+import { ProductProvider } from '../Contexts/productContext'
+import { Cart } from '../pages/cart'
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StandardLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/produto" element={<Product/>} />
-          <Route path="/carrinho" element={<div>Carrinho</div>} />
-          <Route path="/assinatura" element={<div>Assinatura</div>} />
-          <Route path="/agendamento" element={<div>Agendamento</div>} />
-        </Route>
-      </Routes>
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<StandardLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/produto" element={<Product />} />
+            <Route path="/carrinho" element={<Cart/>} />
+            <Route path="/assinatura" element={<div>Assinatura</div>} />
+            <Route path="/agendamento" element={<div>Agendamento</div>} />
+          </Route>
+        </Routes>
+      </ProductProvider>
     </BrowserRouter>
   )
 }
