db3e051aa563ee519b7358b84cda73fc7efa9fbd
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 03:14:17 -0300

    feat/ criação do footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,35 @@
+import { PhoneIcon } from '../../assets/icons/call'
+import { EmailIcon } from '../../assets/icons/email'
+import { LogoEnuxus } from '../../assets/logoEnexus'
+
+export function Footer() {
+  return (
+    <footer className="mt-52 grid h-64 grid-cols-3 items-center rounded-lg bg-[#F8F8F8] px-4 lg:px-36">
+      <nav>
+        <ul className=" flex flex-col gap-4">
+          <li>
+            <a className="flex items-center gap-2 text-fun2 uppercase" href="">
+              <div className=" rounded-md bg-[#F2F2F2] px-6 py-3">
+                <PhoneIcon />
+              </div>
+              Telefone
+            </a>
+          </li>
+          <li>
+            <a className=" flex items-center gap-2 text-fun2 uppercase" href="">
+              <div className=" rounded-md bg-[#F2F2F2] px-6 py-3">
+                <EmailIcon />
+              </div>
+              Email
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className=" flex flex-col items-center justify-center gap-4">
+        <LogoEnuxus />
+        <span className=" text-center">Alguns direitos reservados</span>
+      </div>
+    </footer>
+  )
+}
