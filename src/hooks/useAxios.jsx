f78807717ff260e78e9c8b101d64b7ce028e25a1
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 19:40:30 -0300

    feat/ criando hook useAxios

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import React from 'react'
+
+const useAxios = () => {
+  const [dados, setDados] = React.useState(null)
+  const [erro, setErro] = React.useState(null)
+  const [loading, setLoading] = React.useState(null)
+
+  const requestApi = React.useCallback(async (url, dados, metodo, headers) => {
+    try {
+      setErro(null)
+      setLoading(true)
+      const res = await axios({
+        method: metodo,
+        url,
+        data: dados,
+        headers,
+      })
+      setDados(res.data)
+      return { res, json: res.data }
+    } catch (err) {
+      setErro(err.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  return { dados, loading, erro, requestApi }
+}
+
+export default useAxios
