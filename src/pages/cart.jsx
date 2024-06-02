f46bf222215f8c3e0938a20d2547e4664741b507
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 19:41:23 -0300

    feat/ adicionando página de carrinho

---
+++ src/pages/cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export function Cart() {
+    return (
+        <main className=" flex">
+            <div className="">
+                <p>Produto</p>
+            </div>
+
+        </main>
+    )
+}
