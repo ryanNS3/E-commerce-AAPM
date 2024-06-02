7c7451a7f9bc4031e01af5fa51901d7c6cd2e5ff
ryanNS3 <ryansantosn333@gmail.com>
2024-06-02 03:17:07 -0300

    feat/ adicionando rotas de produto, carrinho, assinatura e agendamento

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { StandardLayout } from '../layout/standardLayout'
 import { Home } from '../pages/home'
+import { Product } from '../pages/product'
 
 export function Router() {
   return (
@@ -8,6 +9,10 @@ export function Router() {
       <Routes>
         <Route path="/" element={<StandardLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/produto" element={<Product/>} />
+          <Route path="/carrinho" element={<div>Carrinho</div>} />
+          <Route path="/assinatura" element={<div>Assinatura</div>} />
+          <Route path="/agendamento" element={<div>Agendamento</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
