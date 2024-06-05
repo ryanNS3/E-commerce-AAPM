a9c2550224c0e2da8649cd9505df6d1b686fccff
ryanNS3 <ryansantosn333@gmail.com>
2024-06-05 01:41:14 -0300

    feat/adicionando notificações do toastify

---
+++ src/Contexts/toastifyContext.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Slide, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export const toastifyContext = React.createContext()
+
+export function ToastifyProvider({ children }) {
+  function Notification(type, message) {
+    switch (type) {
+      case 'success':
+        toast.success(message, {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: 'light',
+          transition: Slide,
+        })
+        break
+
+      case 'loading':
+        toast.loading(message, {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        })
+
+        break
+
+      case 'error':
+        toast.error(message, {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+          transition: Slide,
+        })
+        break
+    }
+  }
+
+  return (
+    <toastifyContext.Provider value={{ Notification }}>
+      {children}
+    </toastifyContext.Provider>
+  )
+}
