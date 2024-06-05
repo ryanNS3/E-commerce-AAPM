29a7d78822f5fd0a2510845c6f21165126bc649c
ryanNS3 <ryansantosn333@gmail.com>
2024-06-05 01:43:43 -0300

    feat/criação do react query

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -7,23 +7,35 @@ import { Cart } from '../pages/cart'
 import { ScrollToTop } from '../layout/scrollToTop'
 import { Login } from '../pages/login'
 import { Scheduling } from '../pages/scheduling'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { UserProvider } from '../Contexts/userContext'
+import { ToastifyProvider } from '../Contexts/toastifyContext'
+import { ToastContainer } from 'react-toastify'
 
 export function Router() {
+  const queryClient = new QueryClient()
   return (
     <BrowserRouter>
-      <ProductProvider>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<StandardLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/produto" element={<Product />} />
-            <Route path="/carrinho" element={<Cart />} />
-            <Route path="/assinatura" element={<div>Assinatura</div>} />
-            <Route path="/agendamento" element={<Scheduling />} />
-          </Route>
-        </Routes>
-      </ProductProvider>
+      <QueryClientProvider client={queryClient}>
+        <ToastifyProvider>
+          <UserProvider>
+            <ProductProvider>
+              <ScrollToTop />
+              <ToastContainer />
+              <Routes>
+                <Route path="/" element={<StandardLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/produto" element={<Product />} />
+                  <Route path="/carrinho" element={<Cart />} />
+                  <Route path="/assinatura" element={<div>Assinatura</div>} />
+                  <Route path="/agendamento" element={<Scheduling />} />
+                </Route>
+              </Routes>
+            </ProductProvider>
+          </UserProvider>
+        </ToastifyProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
