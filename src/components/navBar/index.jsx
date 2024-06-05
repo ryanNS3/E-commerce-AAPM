fe2617fa7698047f2205b779d52df0f17734cb95
ryanNS3 <ryansantosn333@gmail.com>
2024-06-05 10:47:23 -0300

    feat/menu mobile

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -2,41 +2,52 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { LogoEnuxus } from '../../assets/logoEnexus'
 import { CartIcon } from '../../assets/cart'
+import { useMedia } from '../../hooks/useMedia'
+import { MobileMenu } from './menuMobile'
 
 export function NavBar() {
+  const [metchMediaMobile] = useMedia('(max-width:950px)')
+  console.log(metchMediaMobile)
+
   return (
-    <nav aria-label="Navegação primária">
-      <ul className="flex w-full justify-between">
-        <div className="flex gap-8">
-          <li className="text-fun2 hover:text-rosa-300">
-            <Link to={'/'}>Produtos</Link>
-          </li>
-          <li className=" text-fun2">
-            <Link to={'/'}>Sobre</Link>
-          </li>
-        </div>
+    <>
+      {metchMediaMobile ? (
+        <MobileMenu />
+      ) : (
+        <nav aria-label="Navegação primária">
+          <ul className="flex w-full justify-between">
+            <div className="flex gap-8">
+              <li className="text-fun2 hover:text-rosa-300">
+                <Link to={'/'}>Produtos</Link>
+              </li>
+              <li className=" text-fun2">
+                <Link to={'/'}>Sobre</Link>
+              </li>
+            </div>
 
-        <div>
-          <li>
-            <Link to={'/'}>
-              <LogoEnuxus />
-            </Link>
-          </li>
-        </div>
+            <div>
+              <li>
+                <Link to={'/'}>
+                  <LogoEnuxus />
+                </Link>
+              </li>
+            </div>
 
-        <div className="flex items-center gap-8">
-          <li>
-            <Link to={'/carrinho'}>
-              <CartIcon />
-            </Link>
-          </li>
-          <li>
-            <Link to={'/login'}>
-              <div className=" h-11 w-11 rounded-full bg-cinza-100"></div>
-            </Link>
-          </li>
-        </div>
-      </ul>
-    </nav>
+            <div className="flex items-center gap-8">
+              <li>
+                <Link to={'/carrinho'}>
+                  <CartIcon />
+                </Link>
+              </li>
+              <li aria-label='login'>
+                <Link to={'/login'}>
+                  <div className=" h-11 w-11 rounded-full bg-cinza-100"></div>
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      )}
+    </>
   )
 }
