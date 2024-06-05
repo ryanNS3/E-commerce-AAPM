6f081f7e06af83a10ced63fc2c4f23be9aadef6c
ryanNS3 <ryansantosn333@gmail.com>
2024-06-05 01:41:39 -0300

    feat/requisição de todos os produtos

--- src/Contexts/productContext.jsx
+++ src/Contexts/productContext.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import useAxios from '../hooks/useAxios'
+import { useQuery } from '@tanstack/react-query'
 
 export const productContext = React.createContext()
 
 export function ProductProvider({ children }) {
+  const BASE_URL = import.meta.env.VITE_API_URL
+  const { requestAPI } = useAxios()
+
   const sizes = [
     {
       size: 'PP',
@@ -21,8 +26,24 @@ export function ProductProvider({ children }) {
     },
   ]
 
+  async function FetchAllProduct() {
+    const requestApiGetAllProduct = await requestAPI(
+      `${BASE_URL}/produto/todosAtivos`,
+      null,
+      'POST',
+      null,
+    )
+    console.log(requestApiGetAllProduct)
+    return requestApiGetAllProduct
+  }
+
+  const allProductsQuery = useQuery({
+    queryFn: FetchAllProduct,
+    queryKey: ['allProducts'],
+  })
+
   return (
-    <productContext.Provider value={{ sizes }}>
+    <productContext.Provider value={{ sizes, allProductsQuery }}>
       {children}
     </productContext.Provider>
   )
