6a3e9196fa8a7436edf89511ea3ddb94567ac552
ryanNS3 <ryansantosn333@gmail.com>
2024-06-09 01:21:11 -0300

    feat/criação da hook useCookies

---
+++ src/hooks/useCookies.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cookies from 'js-cookie'
+
+export const useCookies = (key, defaultValue, options = {}) => {
+  const [value, setValue] = React.useState(() => {
+    return Cookies.get(key) || defaultValue
+  })
+
+  React.useEffect(() => {
+    Cookies.set(key, value, options)
+  }, [key, value, options])
+
+  return [value, setValue]
+}
