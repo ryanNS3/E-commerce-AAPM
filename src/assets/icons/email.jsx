5ba35fa0a42ea6d43234cfaf701a01e0b64201ed
ryanNS3 <ryansantosn333@gmail.com>
2024-07-24 00:31:54 -0300

    fix/arruamando estilos e bugs

--- src/assets/icons/email.jsx
+++ src/assets/icons/email.jsx
@@ -1,8 +1,8 @@
 export function EmailIcon() {
   return (
     <svg
-      width="20"
-      height="16"
+      width="24"
+      height="24"
       viewBox="0 0 20 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
